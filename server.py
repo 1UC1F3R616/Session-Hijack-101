4c2d2c1cd1a528b7df83dbe856799e62f0062fcb
1UC1F3R616 <kushchoudhary8@gmail.com>
2020-10-22 01:32:21 +0530

    feat: dynamic leaks page

--- server.py
+++ server.py
@@ -1,7 +1,60 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+app.mount("/media", StaticFiles(directory="media/"), name="media")
+templates = Jinja2Templates(directory="templates/")
+
+############################################## utils #########################################
+import cheat_cookie as cc
+
+cj = cc.load()
+
+cjl = [str(x) for x in cj]
+
+tokens = []
+for x in cjl:
+    if 'token' in x:
+        tokens.append(x)
+        # print(x)
+
+sessions = []
+for x in cjl:
+    if 'session' in x.lower():
+        sessions.append(x)
+        # print(x)
+############################################## utils #########################################
+
+@app.get("/home")
+@app.get("/index")
+async def home(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
+
+@app.get("/websites")
+async def websites(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse('websites.html', context={'request': request, 'result': result})
+
+@app.get("/leaks")
+async def leaks(request: Request):
+    result = {}
+    return templates.TemplateResponse('leaks.html', context={'request': request, 'result': result})
+
+@app.get("/leaks/tokens")
+async def leaks(request: Request):
+    result = {'tokens': tokens}
+    return templates.TemplateResponse('leaks.html', context={'request': request, 'result': result})
+
+@app.get("/leaks/sessions")
+async def leaks(request: Request):
+    result = {'sessions': sessions}
+    return templates.TemplateResponse('leaks.html', context={'request': request, 'result': result})
+
+@app.get("/about")
+async def about(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse('about.html', context={'request': request, 'result': result})
